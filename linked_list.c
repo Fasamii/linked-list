004206d0a1fcd6724e25877286f9fd1f399485aa
fasamii <fasami0512@gmail.com>
2024-10-03 17:45:50 +0200

    improved logic of the program

--- linked_list.c
+++ linked_list.c
@@ -6,16 +6,12 @@ typedef struct linked_list {
     struct linked_list *next;
 } linked_list;
 
-void add_to_linkedlist(linked_list *parent, linked_list *new) {
-    while(parent->next != NULL) { parent = parent->next; }
-    parent->next = new;
-}
-
-linked_list *make_link(char *data) {
+void add_to_linkedlist(linked_list *parent, char *data) {
     linked_list *node = malloc(sizeof(linked_list));
     node->data = data;
     node->next = NULL;
-    return node;
+    while(parent->next != NULL) { parent = parent->next; }
+    parent->next = node;
 }
 
 void print_linkedlist(linked_list *node) {
@@ -39,9 +35,9 @@ int main(void) {
     parent->data = "ajaj";
     parent->next = NULL;
 
-    add_to_linkedlist(parent, make_link("fasa"));
-    add_to_linkedlist(parent, make_link("dasa"));
-    add_to_linkedlist(parent, make_link("kasa"));
+    add_to_linkedlist(parent, "fasa");
+    add_to_linkedlist(parent, "kasa");
+    add_to_linkedlist(parent, "masa");
 
     print_linkedlist(parent);
 
